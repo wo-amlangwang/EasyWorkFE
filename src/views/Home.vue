4b64f8013896bfda2d12e1707978fa6da8090ce8
Sonui <me@sonui.cn>
2022-07-21 00:01:36 +0800

    perf: 迁移公共css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <el-row style="height: 100vh;" class="color-background">
-        <div class="content">
+        <div class="content rigth-content">
             <el-col class="hidden-xs-only " :span="18" style="background-color: rgba(0, 0, 0, 0);">
             </el-col>
             <el-col :span="6" class="card">
@@ -14,7 +14,7 @@
     </el-row>
 </template>
 <script setup lang="ts">
-import { useRouter, useRoute } from 'vue-router'
+import { useRouter } from 'vue-router'
 const router = useRouter()
 
 // 设置默认显示的路由
@@ -24,6 +24,15 @@ if (router.currentRoute.value.fullPath === '/')
 
 </script>
 <style>
+.rigth-content {
+    display: flex;
+    min-height: 100vh;
+    width: 100%;
+    text-shadow:
+        0px 0px 1px rgba(255, 255, 255, .6),
+        0px 0px 4px rgba(0, 0, 0, .05);
+}
+
 nav {
     font: 2.4em sans-serif;
     text-align: center;
@@ -53,66 +62,6 @@ nav a {
 
 :root {
     font-size: 15px;
-}
-
-.color-background {
-    margin: 0;
-    min-height: 100vh;
-    /* height: 100%;
-        width: 100%; */
-    /* 能够融合背景图片的颜色 */
-    background-color: #e493D0;
-    /* 背景图片 */
-    background-image:
-        radial-gradient(closest-side, rgba(235, 105, 78, 1), rgba(235, 105, 78, 0)),
-        radial-gradient(closest-side, rgba(243, 11, 164, 1), rgba(243, 11, 164, 0)),
-        radial-gradient(closest-side, rgba(254, 234, 131, 1), rgba(254, 234, 131, 0)),
-        radial-gradient(closest-side, rgba(170, 142, 245, 1), rgba(170, 142, 245, 0)),
-        radial-gradient(closest-side, rgba(248, 192, 147, 1), rgba(248, 192, 147, 0));
-    /* 以屏幕长的一边计算，控制它们的大小 */
-    background-size:
-        130vmax 130vmax,
-        80vmax 80vmax,
-        90vmax 90vmax,
-        110vmax 110vmax,
-        90vmax 90vmax;
-
-    /* 设置位置 */
-    background-position:
-        -80Vmax -80vmax,
-        60Vmax -30vmax,
-        10Vmax 10vmax,
-        -30Vmax -10vmax,
-        50Vmax 50vmax;
-
-    background-repeat: no-repeat;
-    /* 调用动画 */
-    animation: 10s move linear infinite;
-
-}
-
-.color-background::after {
-    content: '';
-    display: block;
-    position: fixed;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    backdrop-filter: blur(10px);
-    -webkit-backdrop-filter: blur(10px);
-}
-
-.content {
-    position: relative;
-    z-index: 99;
-    display: flex;
-    min-height: 100vh;
-    width: 100%;
-    color: transparent;
-    text-shadow:
-        0px 0px 1px rgba(255, 255, 255, .6),
-        0px 0px 4px rgba(0, 0, 0, .05);
 }
 
 @keyframes move {
