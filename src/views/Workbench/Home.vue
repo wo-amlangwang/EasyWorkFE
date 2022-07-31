5fb756e8cf37fe7c4f83bdc42db0dbe85512ad97
Sonui <me@sonui.cn>
2022-07-31 13:01:45 +0000

    feat: 个人信息demo

--- src/views/Workbench/Home.vue
+++ src/views/Workbench/Home.vue
@@ -5,6 +5,13 @@
                 <el-aside>
                     <el-menu class="el-menu-vertical" :collapse="isCollapse" @open="handleOpen" @close="handleClose"
                         @select="handleSelect">
+                        <div class="user">
+                            <el-avatar @click="dialogUser = true" shape="square" class="user-avatar" :size="32"
+                                :fit="'fill'" :src="'/src/assets/avatar.jpg'" />
+                            <div class="user-info">
+                            </div>
+                        </div>
+
                         <el-menu-item v-for="(item, index) in projeckList" :key="index" :index="item.id">
                             <el-icon :size="20">
                                 <Notebook />
@@ -38,6 +45,33 @@
                 </el-main>
             </el-container>
         </el-container>
+        <!-- 个人信息 -->
+        <el-dialog v-model="dialogUser" title="个人信息" width="30%">
+            <div>
+                <div>
+                    用户名:
+                    <img src="" />
+                </div>
+                <div>
+                    邮箱:
+                    <img src="" />
+                </div>
+                <div>
+                    头像:
+                    <img src="" />
+                </div>
+                <div>
+                    头像:
+                    <img src="" />
+                </div>
+            </div>
+            <template #footer>
+                <span class="dialog-footer">
+                    <el-button @click="dialogUser = false">Cancel</el-button>
+                    <el-button type="primary" @click="dialogUser = false">Confirm</el-button>
+                </span>
+            </template>
+        </el-dialog>
     </div>
 </template>
 
@@ -50,6 +84,7 @@ import TaskBoard from '@/views/Workbench/TaskBoard.vue'
 
 const projeckId = ref()
 const isCollapse = ref(false)
+const dialogUser = ref(false)
 let projeckList = [
     {
         icon: 'el-icon-plus',
@@ -88,8 +123,44 @@ const handleClose = (key: string, keyPath: string[]) => {
 </script>
 
 <style>
-.btn-control {
-    
+.user .user-avatar {}
+
+.user {
+    display: flex;
+    align-items: center;
+    height: var(--el-menu-item-height);
+    line-height: var(--el-menu-item-height);
+    font-size: var(--el-menu-item-font-size);
+    color: var(--el-menu-text-color);
+    padding: 0 var(--el-menu-base-level-padding);
+    margin-top: 10px;
+    list-style: none;
+    cursor: default;
+    position: relative;
+    transition: border-color var(--el-transition-duration), background-color var(--el-transition-duration), color var(--el-transition-duration);
+    box-sizing: border-box;
+    white-space: nowrap;
+}
+
+.user .user-info {
+    /* left: 42px;
+    top: 44px;
+    float: right;
+    width: 100px;
+    height: 100px;
+    visibility: hidden;
+    background-color: #000;
+    position: fixed;
+    border-radius: 4px;
+    border: 1px solid #ccc; */
+}
+
+.user .user-avatar:hover~.user-info {
+    visibility: visible;
+}
+
+.user .user-avatar:hover {
+    cursor: pointer;
 }
 
 .el-container,
@@ -100,6 +171,8 @@ const handleClose = (key: string, keyPath: string[]) => {
 
 .el-menu-vertical {
     height: 100%;
+    display: flex;
+    flex-direction: column;
 }
 
 .el-menu-vertical button {
