c81180382182e8029feca989ce4ca7d1abfc7575
Sonui <me@sonui.cn>
2022-08-03 16:30:45 +0000

    perf: 整合后端接口

--- src/utils/EasyWorkAPI.ts
+++ src/utils/EasyWorkAPI.ts
@@ -1,4 +1,7 @@
 import axios from "axios";
+import type { UserInfo } from '@/utils/Model';
+
+const prefix = "api";
 
 const EasyWorkAPI = {
     /**
@@ -9,18 +12,17 @@ const EasyWorkAPI = {
      * @param password 密码
      * @returns 成功返回字符串，失败返回错误原因
      */
-    signup: function(name: string, email: string, password: string): Promise<string> {
+    signup: function (name: string, email: string, password: string): Promise<string> {
         return new Promise((resolve, reject) => {
-            axios('/api/reguser', {
+            axios(prefix + '/api/reguser', {
                 method: 'POST',
                 data: {
                     username: name,
-                    email: email,
                     password: password
-                }
+                },
             }).then(res => {
-                if (res.data.success) {
-                    resolve(res.data.data);
+                if (res.data.status === 0) {
+                    resolve(res.data);
                 } else {
                     reject(res.data.message);
                 }
@@ -37,20 +39,22 @@ const EasyWorkAPI = {
      * @param veify 验证码
      * @returns 登录结果
      */
-    login: function(username: string, password: string, veify: string): Promise<string> {
+    login: function (username: string, password: string, veify: string): Promise<string> {
         return new Promise((resolve, reject) => {
-            axios('/api/login', {
+            axios(prefix + '/api/login', {
                 method: 'POST',
                 data: {
                     username: username,
                     password: password,
-                    veify: veify
-                }
+                },
             }).then(res => {
-                if (res.data.success) {
-                    resolve(res.data.token);
+                console.log(res);
+                if (res.data.status == 0) {
+                    let token = res.data.token.substring(7, res.data.token.length);
+                    localStorage.setItem('token', token);
+                    resolve(res.data);
                 } else {
-                    reject(res.data.message);
+                    reject(res.data);
                 }
             }).catch(err => {
                 reject(err);
@@ -64,17 +68,20 @@ const EasyWorkAPI = {
      * @param email 邮箱
      * @returns 
      */
-    updateUser: function(nickname: string, email: string): Promise<string> {
+    updateUser: function (nickname: string, email: string): Promise<string> {
         return new Promise((resolve, reject) => {
-            axios('/my/userinfo', {
+            axios(prefix + '/my/userinfo', {
                 method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('token')
+                },
                 data: {
                     nickname: nickname,
                     email: email
                 }
             }).then(res => {
-                if (res.data.success) {
-                    resolve(res.data.data);
+                if (res.data.status === 0) {
+                    resolve(res.data.message);
                 } else {
                     reject(res.data.message);
                 }
@@ -88,13 +95,23 @@ const EasyWorkAPI = {
      * 
      * @returns 用户信息
      */
-    getUser: function(): Promise<string> {
+    getUserInfo: function (): Promise<UserInfo | string> {
         return new Promise((resolve, reject) => {
-            axios('/my/userinfo', {
-                method: 'GET'
+            axios(prefix + '/my/userinfo', {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('token')
+                }
             }).then(res => {
-                if (res.data.success) {
-                    resolve(res.data.data);
+                if (res.data.status == 0) {
+                    const ret: UserInfo = {
+                        id: res.data.data.id,
+                        username: res.data.data.username,
+                        nickname: res.data.data.nickname,
+                        email: res.data.data.email,
+                        user_pic: res.data.data.user_pic
+                    }
+                    resolve(ret);
                 } else {
                     reject(res.data.message);
                 }
@@ -111,18 +128,21 @@ const EasyWorkAPI = {
      * @param repwd 重复新密码
      * @returns 修改结果
      */
-    updatePwd: function(oldpwd: string, newpwd: string, repwd: string): Promise<string> {
+    updatePwd: function (oldpwd: string, newpwd: string, repwd: string): Promise<string> {
         return new Promise((resolve, reject) => {
-            axios('/my/updatepwd', {
+            axios(prefix + '/my/updatepwd', {
                 method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('token')
+                },
                 data: {
                     oldPwd: oldpwd,
                     newPwd: newpwd,
                     rePwd: repwd
                 }
             }).then(res => {
-                if (res.data.success) {
-                    resolve(res.data.data);
+                if (res.data.status === 0) {
+                    resolve(res.data.message);
                 } else {
                     reject(res.data.message);
                 }
@@ -137,15 +157,18 @@ const EasyWorkAPI = {
      * @param avatar 头像 图片base64
      * @returns 修改结果
      */
-    updateAvatar: function(avatar: string): Promise<string> {
+    updateAvatar: function (avatar: string): Promise<string> {
         return new Promise((resolve, reject) => {
-            axios('/my/update/avatar', {
+            axios(prefix + '/my/update/avatar', {
                 method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('token')
+                },
                 data: {
                     avatar: avatar
                 }
             }).then(res => {
-                if (res.data.success) {
+                if (res.data.status === 0) {
                     resolve(res.data.data);
                 } else {
                     reject(res.data.message);
---
+++ src/utils/Model.ts
@@ -0,0 +1,12 @@
+interface UserInfo {
+    id: number
+    username: string
+    nickname: string
+    email: string
+    user_pic: string
+}
+
+
+export type {
+    UserInfo
+}
