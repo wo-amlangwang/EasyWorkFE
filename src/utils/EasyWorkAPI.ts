35844717c33417a544db53b3c766a4ecf6faf6eb
Sonui <me@sonui.cn>
2022-08-05 14:13:13 +0000

    feat: 项目接口 用户接口

--- src/utils/EasyWorkAPI.ts
+++ src/utils/EasyWorkAPI.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
-import type { UserInfo } from '@/utils/Model';
+import type { UserInfo, ProjectInfo, TaskInfo, TimeLine } from '@/utils/Model';
+import { rest } from "lodash";
 
 const prefix = "api";
 
-const EasyWorkAPI = {
+const user = {
     /**
      * 用户注册
      * 
@@ -68,7 +69,7 @@ const EasyWorkAPI = {
      * @param email 邮箱
      * @returns 
      */
-    updateUser: function (nickname: string, email: string): Promise<string> {
+    updateNickNameAndEmail: function (nickname: string, email: string): Promise<string> {
         return new Promise((resolve, reject) => {
             axios(prefix + '/my/userinfo', {
                 method: 'POST',
@@ -128,7 +129,7 @@ const EasyWorkAPI = {
      * @param repwd 重复新密码
      * @returns 修改结果
      */
-    updatePwd: function (oldpwd: string, newpwd: string, repwd: string): Promise<string> {
+    updatePassword: function (oldpwd: string, newpwd: string, repwd: string): Promise<string> {
         return new Promise((resolve, reject) => {
             axios(prefix + '/my/updatepwd', {
                 method: 'POST',
@@ -178,6 +179,408 @@ const EasyWorkAPI = {
             });
         });
     }
+};
+
+const project = {
+    /**
+     * 创建项目
+     * 
+     * @param project_name 项目名称
+     * @param project_details 项目说明
+     */
+    create: (project_name: string, project_details: string): Promise<string> => {
+        return new Promise((resolve, reject) => {
+            axios(prefix + '/project/create', {
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('token')
+                },
+                data: {
+                    project_name: project_name,
+                    project_details: project_details
+                },
+            }).then(res => {
+                if (res.data.status === 0) {
+                    resolve(res.data);
+                } else {
+                    reject(res.data.message);
+                }
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    },
+    /**
+     * 获取项目列表
+     */
+    getList: (): Promise<string | ProjectInfo[]> => {
+        return new Promise((resolve, reject) => {
+            axios(prefix + '/project/projectlist', {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('token')
+                }
+            }).then(res => {
+                if (res.data.status == 0) {
+                    let ret: ProjectInfo[] = new Array();
+                    res.data.data.forEach((item: any) => {
+                        ret.push({
+                            id: item.id,
+                            name: item.p_name,
+                            details: '',
+                            master: item.members,
+                            create_time: '',
+                            deleted: item.deleted,
+                        })
+                    });
+                    resolve(ret);
+                } else {
+                    reject(res.data.message);
+                }
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    },
+
+    /**
+     * 添加项目成员
+     * 
+     * @param projectname 项目名称
+     * @param member 用户名
+     */
+    addMember: (projectname: string, member: string): Promise<string> => {
+        return new Promise((resolve, reject) => {
+            axios(prefix + '/project/addmember', {
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('token')
+                },
+                data: {
+                    projectname: projectname,
+                    member: member
+                },
+            }).then(res => {
+                if (res.data.status === 0) {
+                    resolve(res.data);
+                } else {
+                    reject(res.data.message);
+                }
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    },
+    /**
+     * 删除项目
+     * 
+     * @param projectname 项目名称
+     */
+    delete: (projectname: string): Promise<string> => {
+        return new Promise((resolve, reject) => {
+            axios(prefix + '/project/deleteproject', {
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('token')
+                },
+                data: {
+                    project_name: projectname,
+                },
+            }).then(res => {
+                if (res.data.status === 0) {
+                    resolve(res.data);
+                } else {
+                    reject(res.data.message);
+                }
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    },
+
+    /**
+     * 获取项目详情
+     * 
+     * @param projectname 项目名称
+     */
+    getInfo: (projectname: string): Promise<string | ProjectInfo> => {
+        return new Promise((resolve, reject) => {
+            axios(prefix + '/project/getproject', {
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('token')
+                }
+            }).then(res => {
+                if (res.data.status == 0) {
+                    const ret: ProjectInfo = {
+                        id: res.data.data.id,
+                        name: res.data.data.p_name,
+                        details: res.data.data.project_details,
+                        master: res.data.data.create_user,
+                        create_time: res.data.data.create_time,
+                        deleted: res.data.data.deleted,
+                    };
+                    resolve(ret);
+                } else {
+                    reject(res.data.message);
+                }
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    },
+
+    /**
+     * 更新项目说明
+     * 
+     * @param projectname 项目名称
+     * @param project_details 项目说明
+     */
+    update: (projectname: string, project_details: string): Promise<string> => {
+        return new Promise((resolve, reject) => {
+            axios(prefix + '/project/updateproject', {
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('token')
+                },
+                data: {
+
+                },
+            }).then(res => {
+                if (res.data.status === 0) {
+                    resolve(res.data);
+                } else {
+                    reject(res.data.message);
+                }
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    },
+
+    /**
+     * 获取项目成员
+     * 
+     * @param projectname 项目名称
+     */
+    getMembers: (projectname: string): Promise<string | string[]> => {
+        return new Promise((resolve, reject) => {
+            axios(prefix + '/project/projectmemberlist', {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('token')
+                }
+            }).then(res => {
+                if (res.data.status == 0) {
+                    let members: string[] = new Array();
+                    res.data.data.forEach((item: any) => {
+                        members.push(item.members);
+                    });
+                    resolve(members);
+                } else {
+                    reject(res.data.message);
+                }
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    },
+
+    /**
+     * 获取任务列表
+     * 
+     * @param projectname 项目名称
+     */
+    getTaskList: (projectname: string): Promise<string | TaskInfo[]> => {
+        return new Promise((resolve, reject) => {
+            axios(prefix + '/project/projecttasklist', {
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('token')
+                },
+                data: {
+                    project_name: projectname,
+                },
+            }).then(res => {
+                if (res.data.status == 0) {
+                    let tasks: TaskInfo[] = new Array();
+                    res.data.data.forEach((item: any) => {
+                        tasks.push({
+                            id: item.id,
+                            name: item.task_name,
+                            project_name: item.project_name,
+                            details: item.task_details,
+                            type: item.type,
+                            create_user: item.create_user,
+                            create_time: item.create_time,
+                            update_user: item.update_user,
+                            update_time: item.update_time,
+                            priority: item.priority,
+                            deadline: item.deadline,
+                            assignee: [item.members],
+                            status: item.status,
+                            task_comment: item.task_comment,
+                            deleted: item.deleted,
+                            time_line: new Array(),
+                        });
+                    });
+                    resolve(tasks);
+                } else {
+                    reject(res.data.message);
+                }
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    }
+}
+
+const task = {
+    /**
+     * 新建任务
+     * 
+     * @param info 任务信息
+     */
+    craete: (info: TaskInfo): Promise<string> => {
+        return new Promise((resolve, reject) => {
+            axios(prefix + '/project/create', {
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('token')
+                },
+                data: {
+                    task_name: info.name,
+                    task_details: info.details,
+                    project_name: info.project_name,
+                    type: info.type,
+                    priority: info.priority,
+                    deadline: info.deadline,
+                    assignee: info.assignee,
+                    task_comment: info.task_comment
+                },
+            }).then(res => {
+                if (res.data.status === 0) {
+                    resolve(res.data);
+                } else {
+                    reject(res.data.message);
+                }
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    },
+
+    /**
+     * 删除任务
+     * 
+     * @param id 任务id
+     */
+    delete: (id: number): Promise<string> => {
+        return new Promise((resolve, reject) => {
+            axios(prefix + '/task/deletetask', {
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('token')
+                },
+                data: {
+                    id: id,
+                },
+            }).then(res => {
+                if (res.data.status === 0) {
+                    resolve(res.data);
+                } else {
+                    reject(res.data.message);
+                }
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    },
+    
+    /**
+     * 获取特定类型的任务
+     * 
+     * @param projectname 项目名称
+     * @param type 任务类型
+     */
+    getListByType: (projectname: string, type: number): Promise<string | TaskInfo[]> => {
+        return new Promise((resolve, reject) => {
+            axios(prefix + '/task/tasktypelist/' + type, {
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('token')
+                },
+                data: {
+                    project_name: projectname,
+                },
+            }).then(res => {
+                if (res.data.status == 0) {
+                    let tasks: TaskInfo[] = new Array();
+                    res.data.data.forEach((item: any) => {
+                        tasks.push({
+                            id: item.id,
+                            name: item.task_name,
+                            project_name: item.project_name,
+                            details: item.task_details,
+                            type: item.type,
+                            create_user: item.create_user,
+                            create_time: item.create_time,
+                            update_user: item.update_user,
+                            update_time: item.update_time,
+                            priority: item.priority,
+                            deadline: item.deadline,
+                            assignee: [item.members],
+                            status: item.status,
+                            task_comment: item.task_comment,
+                            deleted: item.deleted,
+                            time_line: new Array(),
+                        });
+                    });
+                    resolve(tasks);
+                } else {
+                    reject(res.data.message);
+                }
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    },
+
+    /**
+     * 修改任务状态
+     * 
+     * @param projectname 项目名称
+     * @param id 任务ID
+     * @param status 状态
+     */
+    updateStatus: (projectname: string, id: number, status: number): Promise<string> => {
+        return new Promise((resolve, reject) => {
+            axios(prefix + '/task/updatetaskstatus/' + status, {
+                method: 'POST',
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('token')
+                },
+                data: {
+                    project_name: projectname,
+                    id: id,
+                },
+            }).then(res => {
+                if (res.data.status === 0) {
+                    resolve(res.data);
+                } else {
+                    reject(res.data.message);
+                }
+            }).catch(err => {
+                reject(err);
+            });
+        });
+    }
+
+}
+const EasyWorkAPI = {
+    user: user,
+    project: project,
+    task: task
 }
 
 export default EasyWorkAPI;
